6454e6ab6b369ee21763b665ff0224c8afdf8461
ornaverum <waddellj@asmsa.org>
2024-08-21 19:31:53 -0500

    feat: Add Canvas component for video analysis page

---
+++ src/Components/Canvas.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import {onMount} from 'svelte';
+
+    export let name:string = 'Canvas'
+
+    export let width:number = 300
+	export let height:number = 300
+	export let background:string = '#fff'
+	
+	let canvas;
+	let context
+
+
+</script>
+
+
+<div>
+    <canvas bind:this={canvas} width={width} height={height} class='bg-primary-200'></canvas>
+</div>
--- src/routes/vid-analysis/+page.svelte
+++ src/routes/vid-analysis/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import Canvas from '../../Components/Canvas.svelte';
+
+	let canvas;
+</script>
+
 <main class='flex flex-col items-center h-3/4'>
 	<h1 class = 'text-2xl h-3/4'>
 		Coming soon...
@@ -5,4 +11,5 @@
 	<p>
 		I recommend <a href="https://noragulfa.com/sigmund/"> Noragulfa's Sigmund Video Analysis</a> for video analysis.
 	</p>
+	<Canvas width={300} height={300} background='#aaa'/>
 </main>
