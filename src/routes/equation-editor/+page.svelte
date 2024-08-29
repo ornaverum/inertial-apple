aa00b136e622f070fc3d0dbb52f2659b83ebeb29
ornaverum <waddellj@asmsa.org>
2024-08-29 10:54:40 -0500

    chore: Update mathquill dependency to version 0.10.1-a

--- src/routes/equation-editor/+page.svelte
+++ src/routes/equation-editor/+page.svelte
@@ -2,6 +2,17 @@
 	import Canvas from '../../Components/Canvas.svelte';
 	import Katex from 'svelte-katex';
 	let canvas;
+	var MQ = MathQuill.getInterface(2);
+
+	var htmlElement = document.getElementById('some_id');
+	var config = {
+		handlers: { edit: function(){ ... } },
+		restrictMismatchedBrackets: true
+	};
+	var mathField = MQ.MathField(htmlElement, config);
+
+	mathField.latex('2^{\\frac{3}{2}}'); // Renders the given LaTeX in the MathQuill field
+	mathField.latex(); // => '2^{\\frac{3}{2}}'
 </script>
 
 <main class='flex flex-col items-center h-3/4'>
@@ -12,5 +23,7 @@
 	Here is my favorite equation, <Katex displayMode="false">ax^2+bx+c=0</Katex>.
 	<!--display math example-->
 	<Katex displayMode>ax^2+bx+c=0</Katex>
+
+	
 	
 </main>
