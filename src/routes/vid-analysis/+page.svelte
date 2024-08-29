20b8a9a9da4de90257d39acba6f9824eba702991
ornaverum <waddellj@asmsa.org>
2024-08-29 14:37:49 -0500

    feat: Update video analysis page with Canvas component and file upload functionality

--- src/routes/vid-analysis/+page.svelte
+++ src/routes/vid-analysis/+page.svelte
@@ -1,15 +1,64 @@
 <script lang="ts">
 	import Canvas from '../../Components/Canvas.svelte';
-
+	import { Video, Fileupload, Label } from 'flowbite-svelte'
+	import { Stage, Layer, Line, Circle, Arrow, Image} from 'svelte-konva';
+	import {onMount} from 'svelte';
+	let videoFile:string = '/assets/RunningShort.mp4';
 	let canvas;
+	let height:number = 640;
+	let width:number = 480;
+	let image = null;
+	let video = null;
+
+    onMount(() => {
+        const img = document.createElement("img");
+        img.src = "https://konvajs.org/assets/yoda.jpg";
+        img.onload = () => {
+            image = img;
+        };
+		const vid = document.createElement("vid");
+		vid.src =  '/assets/RunningShort.mp4';
+		vid.onload = () => {
+			video = vid;
+		};
+    });
 </script>
 
 <main class='flex flex-col items-center h-3/4'>
-	<h1 class = 'text-2xl h-3/4'>
-		Coming soon...
-	</h1>
-	<p>
-		I recommend <a href="https://noragulfa.com/sigmund/"> Noragulfa's Sigmund Video Analysis</a> for video analysis.
-	</p>
-	<Canvas width={300} height={300} background='#aaa'/>
+
+
+	<div id="container" class="w-8/12 bg-gray-500 mx-auto my-4 relative">
+		<Stage config={{width, height, id:'main_stage'}}>
+			<Layer>
+				<video
+					id="videoInput"
+					width="640"
+					height="480"
+					controls
+					muted
+					src={videoFile}
+					class='mx-auto absolute top-0 left-0'
+				></video>
+
+			</Layer>
+			<Layer>
+				<Line config = {{
+					points:[0, 0, width, height],
+					stroke:"red",
+					strokeWidth:10
+					}}/>
+			</Layer>
+		</Stage>
+	</div>
+
+	<div>
+		<Label class="space-y-2 mb-2">
+			<span>Upload file</span>
+			<Fileupload bind:value={videoFile} 
+				on:change={()=>{console.log(videoFile)}}
+			/>
+		</Label>
+		<Label>File: {videoFile}</Label>
+	</div>
+	
 </main>
