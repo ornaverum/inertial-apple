e912f28da74f68111c66bfff6c73d5ab64c7d872
ornaverum <waddellj@asmsa.org>
2024-09-23 11:56:15 -0500

    update video load async and sizing

--- src/routes/vid-analysis/+page.svelte
+++ src/routes/vid-analysis/+page.svelte
@@ -10,41 +10,36 @@
 	import KonvaVidEx from './konvaVidEx.svelte';
 
 	let btn:Button;
-	console.log('script');
-	console.log(btn);
 	let stage:Konva.Stage;
 	let layer:Konva.Layer;
-	let image;
 	let video:HTMLVideoElement;
 	let anim:Konva.Animation;
+	let height: number = 500;
+	let width: number = 500;
 
 	onMount(async () => {
 		console.log('Mounted');
-		console.log(btn);
-		console.log(stage);
 		await tick();
-		console.log(layer);
-
-		const img = document.createElement("img");
-        img.src = "https://konvajs.org/assets/yoda.jpg";
-        img.onload = () => {
-            image = img;
-        };
-
-
 		
 	  	video = document.createElement('video');
-
 		video.src = 'assets/Running.MOV';
+
+		async function loadVideo() {
+			await video.play();
+			video.pause();
+		}
+		await loadVideo();
+		height = video.videoHeight || 500;
+		width = video.videoWidth || 500;
+
 		const drawFrame = ()=>{
 			let canvas=layer.canvas;
 			let ctx = canvas.context;
-			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+			ctx.drawImage(video, 0, 0, width, height);
 		}
 		drawFrame();
 
 		video.addEventListener("play", () => {
-			
 			function step() {
 				drawFrame();
 				requestAnimationFrame(step);
@@ -56,13 +51,13 @@
 </script>
 
 <main class='flex flex-col items-center h-3/4'>
-	<Button on:click={() => {video.play(); anim.start(); }} bind:this={btn}>
+	<Button on:click={() => {video.play(); }} bind:this={btn}>
 		<PlayOutline />
 	</Button>
-	<Button on:click={() =>  {video.pause(); anim.stop(); }} bind:this={btn}>
+	<Button on:click={() =>  {video.pause(); }} bind:this={btn}>
 		<PauseOutline />
 	</Button>
-	<Stage config={{height: 500, width:500}} bind:handle={stage}>
+	<Stage config={{height:600, width:800}} bind:handle={stage}>
 		<Layer bind:handle={layer}>
 			<!-- <Image config={{image:image, draggable:true, x:50, y:100}} /> -->
 		</Layer>
